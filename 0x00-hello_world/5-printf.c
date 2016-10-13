3beceec87a8260398cdfac06bc3c4c6ec10717a1
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-13 02:30:25 +0000

    5 no comment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-/**
- * main - prints string using printf
- * Return 0 to exit
- */
 
-int Main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
