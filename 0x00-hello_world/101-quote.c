be5ac9fb4e985f8a6fe743d836dd1f20702df429
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-13 05:01:08 +0000

    ex101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print to standard error
+ * Return: 1 to exit
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
