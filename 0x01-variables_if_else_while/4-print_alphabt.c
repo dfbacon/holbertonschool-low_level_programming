5b33c92a4c4f096460a2ee6923fed57cc734e3f5
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-13 21:06:53 +0000

    ex4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet except q and e
+ * Return: 0 to exit
+ */
+
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		if (l != 'e' && l != 'q')
+		{
+			putchar(l);
+		}
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
