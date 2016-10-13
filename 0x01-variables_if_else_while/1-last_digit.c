7bbb1c7656af9e42ed2d2efd75b600e833ca5778
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-13 19:50:27 +0000

    ex1 ld variable create

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -17,10 +18,12 @@ int main(void)
 	if (ld > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
-	} else if (ld == 0)
+	}
+	else if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
-	} else if (ld < 6 && ld != 0)
+	}
+	else if (ld < 6 && ld != 0)
 	{
 		printf("Last digit of %d is %d and is lass than 6 and not 0\n", n, ld);
 	}
