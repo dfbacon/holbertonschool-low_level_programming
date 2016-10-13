5cf7c1ca9e14aaaf560e9296eda220178c4e954a
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-13 19:46:21 +0000

    ex1 variable name change

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	x = n % 10;
-	if (x > 5)
+	ld = n % 10;
+	if (ld > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if (x == 0)
+	else if (ld == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, x);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-	else
+	else if (ld < 6 && ld != 0)
 	{
-		printf("Last digit of %d is %d and is lass than 6 and not 0\n", n,x);
+		printf("Last digit of %d is %d and is lass than 6 and not 0\n", n, ld);
 	}
 	return (0);
 }
