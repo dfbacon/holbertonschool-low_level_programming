c58838e4638ddc75bdefd6c9a8bff0b7fe6adcde
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-13 19:33:53 +0000

    ex2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet on single line
+ * Return: 0 to exit
+ */
+
+int main(void)
+{
+	char l;
+
+	while (l = 'a'; l <= 'z')
+	{
+		putchar(x);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
