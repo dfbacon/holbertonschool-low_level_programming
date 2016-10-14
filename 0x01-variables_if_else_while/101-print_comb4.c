bd5bc373caf53ef2410770411ae8315b80b33034
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-14 04:56:09 +0000

    ex 101 remove semicolon

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,18 +2,18 @@
 
 /**
  * main - print unique 3 digit combos
- * Return: 0 to exit
+ * Return: 0
  */
 
-int main(void);
+int main(void)
 {
 	int i;
 	int n;
 	int x;
 
 	i = 0;
-	n = i + 1;
-	x = n + 1;
+	/*n = i + 1;*/
+	/*x = n + 1;*/
 
 	while (i < 10)
 	{
@@ -28,8 +28,8 @@ int main(void);
 				putchar(x + '0');
 				if (i + n + x != 24)
 				{
-					printchar(',');
-					printchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 				x++;
 			}
