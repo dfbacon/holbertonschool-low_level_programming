9d3e771a9f3efa2f8aae3c03eb320bccedde3df1
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-18 21:33:48 +0000

    ex5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,9 +4,10 @@
 /**
  * print_sign - prints the sign of a number
  * @n: number in question
- * Return: 1 if positive
- * Return: 0 if zero
- * Return: -1 if negative
+ *
+ * Return: 1 if n is positive
+ * Returns 0 if n is zero
+ * Returns -1 if n is negative
  */
 
 int print_sign(int n)
@@ -14,16 +15,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return(0);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }
