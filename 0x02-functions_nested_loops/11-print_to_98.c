6a8f51f40f1194015314f9014046e4831ce6119d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-18 23:43:55 +0000

    ex11 simplified

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,27 +11,16 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (i = n; i < 99; i++)
-		{
-			if (i == 98)
-			{
-				printf('\n');
-			}
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
-		}
 
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
-		for (i =n; i > 97; i--)
-		{
-			if (i == 98)
-			{
-				printf('\n');
-			}
+		for (i =n; i > 98; i--)
 			printf("%d, ", i);
-		}
 	}
+	printf("98\n");
 }
