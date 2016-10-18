032da38bbd3df7bd248b669c8e0e46751782d58c
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-18 23:20:24 +0000

    ex10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * add - add two integers
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: result of addition
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return(z);
+}
