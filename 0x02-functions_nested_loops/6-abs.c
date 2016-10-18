b3a81f19dd4b36d2ca8ef5f105f79af1db7747b7
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-18 21:41:48 +0000

    ex6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _abs - compute the absolute value
+ * @int: integer in question
+ *
+ * Return: 0
+ */
+
+in _abs(int)
+{
+	int n;
+	int p;
+
+	if (int >= 0)
+	{
+		_putchar(int);
+	}
+	else if (int < 0)
+	{
+		n = int;
+		p = -n;
+		_putchar(p);
+	}
+	return(0);
+}
