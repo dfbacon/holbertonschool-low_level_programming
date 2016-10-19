88bbef8e910d17fd4644bb8e6c1c4b9461108a59
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-19 23:15:08 +0000

    ex103 fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,17 @@
 
 int main(void)
 {
-	enum { MAX_VALUE = 4000000 };
 	long i, n, f, sum;
 
 	i = 0;
 	n = 1;
-	while((f = i + n) < MAX_VALUE)
+	while((f = i + n) < 4000000)
 	{
 		if (f % 2 == 0)
 			sum += f;
 		i = n;
 		n = sum;
 	}
-	printf("%d\n", sum);
+	printf("%ld\n", sum);
 	return (0);
 }
