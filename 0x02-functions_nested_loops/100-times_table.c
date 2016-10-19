817ab26c4c68489154142510969b58f62adb4796
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-19 19:57:21 +0000

    ex100 reformat

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,45 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
- * print_times_table - print times table for n
- * @n: number to be checked
- *
+ * print_times_table - prints n times table
+ * @n: argument used to define size of multiplcation table
  * Return: void
  */
 
 void print_times_table(int n)
 {
-	int x;
-	int y;
+	int x, y;
 
-	if (n >= 0 || n <= 15)
+	if (!(n < 0 || n > 15))
 	{
 		for (x = 0; x <= n; x++)
 		{
-			for(y = 0; y <= n; y++)
+			for (y = 0; y <= n; y++)
 			{
 				if (y == 0)
 				{
-					_putchar(y * x + '0')
+					_putchar(y * x + '0');
 				}
 				else if ((y * x) < 10)
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(x * y + '0')
+					_putchar(x * y + '0');
 				}
-				else if ((y * x) < 100)
+				else if ((x * y) < 100)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((y * x) / 10 + '0');
-					_putchar((y * x) % 10 + '0');
+					_putchar((x * y) / 10 + '0');
+					_putchar((x * y) % 10 + '0');
 				}
 				else if ((x * y) > 99)
 				{
 					_putchar(' ');
 					_putchar((x * y) / 100 + '0');
 					_putchar(((x * y) / 10) % 10 + '0');
-					_putchar((x * y) % 10 + '0';
+					_putchar((x * y) % 10 + '0');
 				}
 				if (y != n)
 					_putchar(',');
