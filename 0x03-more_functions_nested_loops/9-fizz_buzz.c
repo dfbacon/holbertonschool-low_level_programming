48e2331336042d188d59bc98fd3307a59cd98022
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-21 07:22:33 +0000

    ex9

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,5 +9,24 @@
 
 int main(void)
 {
-	for
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 1)
+			printf("1");
+
+		else if (((i % 3) == 0) && ((i % 5) == 0))
+			printf(" FizzBuzz");
+
+		else if ((i % 3) == 0)
+			printf(" Fizz");
+
+		else if ((i % 5) == 0)
+			printf(" Buzz");
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
+	return (0);
 }
