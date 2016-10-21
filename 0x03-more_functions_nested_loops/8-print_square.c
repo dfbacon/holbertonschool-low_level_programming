2ecd6d084783c2bff4c330e437a0f9365d636559
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-21 07:17:22 +0000

    ex8 redo

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_square - print # in size l x w
- * @size: size of the square
+ * print_square - print a square from # symbols
+ * @size: size of square
  *
  * Return: void
  */
@@ -12,15 +12,15 @@ void print_square(int size)
 	int x;
 	int y;
 
-	if (size > 0)
+	if (size <= 0)
+		_putchar('\n');
+	else
 	{
 		for (x = 0; x < size; x++)
 		{
-			for(y = 0; y < size; y++)
+			for (y = 0; y < size; y++)
 				_putchar('#');
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
 }
