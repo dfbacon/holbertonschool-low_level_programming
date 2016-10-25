4fda7ec878863f6eeb1cf6d428414bb1223a811a
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-25 17:49:56 +0000

    ex3

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * _puts - print string with trailing newline to stdout
- * @str: pointer to string
+ * _puts - print string with trailing new line
+ * @str: string to print
  *
  * Return: void
  */
@@ -12,9 +12,7 @@ void _puts(char *str)
 	char s;
 
 	while ((s = *str) != '\0')
-        {
 		_putchar(s);
-		*str++;
-        }
+	*str++;
 	_putchar('\n');
 }
