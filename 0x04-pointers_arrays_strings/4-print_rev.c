2907e7124cf876b501de41c118557955162e1a4d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-25 19:24:33 +0000

    ex4

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_rev - print a string in reverse with trailing new line
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != 0)
+		i++;
+	i -= 1;
+	while (i > 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
