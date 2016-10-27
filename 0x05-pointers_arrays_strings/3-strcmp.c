7f2db51a455a699fe4fd5cfc26021bd457f1f86d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-27 20:19:06 +0000

    ex3 new approach even though last code worked

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int sl1, sl2;
+	int i;
 
-	sl1 = sl2 = 0;
-	while (s1[sl1] != '\0')
-		sl1++;
-	while (s2[sl2] != '\0')
-		sl2++;
-	if (sl1 < sl2)
-		return (-1);
-	else if (sl1 > sl2)
-		return (1);
-	else
-		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] >  s2[i])
+			return (s1[i] - s2[i]);
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
