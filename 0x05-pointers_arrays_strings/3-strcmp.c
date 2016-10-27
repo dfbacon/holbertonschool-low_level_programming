44a5f83d28558306502850885de2a038db98129d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-27 20:16:09 +0000

    ex3 change

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -5,23 +5,18 @@
  * @s1: string one
  * @s2: string two
  *
- * Return: int
+ * Return: -1, 1, or 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int sl1, sl2;
 
-	sl1 = 0;
-	sl2 = 0;
+	sl1 = sl2 = 0;
 	while (s1[sl1] != '\0')
-	{
 		sl1++;
-	}
 	while (s2[sl2] != '\0')
-	{
 		sl2++;
-	}
 	if (sl1 < sl2)
 		return (-1);
 	else if (sl1 > sl2)
