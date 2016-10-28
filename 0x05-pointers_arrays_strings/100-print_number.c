d76300e455534caac4ab0dcf67b4486728826d40
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-28 19:37:37 +0000

    ex100

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
 #include "holberton.h"
+
+/**
+ * print_number - print an integer
+ * @n: number to print
+ *
+ * Return: nothing
+ */
 
 void print_number(int n)
 {
-
+	if (n < 0)
+		_putchar('-');
+	else
+		n = -n;
+	if ((n / 10) < 0)
+		print_number(-(n / 10));
+	_putchar('0' - n % 10);
 }
