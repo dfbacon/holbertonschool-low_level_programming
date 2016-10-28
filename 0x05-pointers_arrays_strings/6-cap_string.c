e74a9eb2e87dc7178737f291159766b77b23bd8c
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-28 03:41:21 +0000

    ex6 opening if

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,8 @@ char *cap_string(char *s)
 {
 	int i;
 
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
