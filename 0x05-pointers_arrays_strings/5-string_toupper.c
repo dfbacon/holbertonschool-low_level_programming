917537f1ed600c722c946170dab452ffbd5fba75
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-28 03:29:46 +0000

    ex5

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * string_toupper - change lowercase letters to upper
+ * @c: pointer to char
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if(c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+	}
+	return(c);
+}
