005dcaaf1b6ad34b15ab60c070effba881828f29
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-10-31 20:55:56 +0000

    ex3 remove comments

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -12,15 +12,10 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	/* iterate to get bytes from accept */
 	i = 0;
 	while (accept[i] != '\0')
 		i++;
-
-	/* increase i by 1 to include \0 */
 	i = i + 1;
-
-	/* iterate s until i reached */
 	while (j < i)
 	{
 		if (s[j] != '\0')
