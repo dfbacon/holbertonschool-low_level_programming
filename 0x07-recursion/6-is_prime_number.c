3a66146eaca9908057ad7c3db709c038bc130e92
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-02 22:22:58 +0000

    ex6 incomplete

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -9,5 +9,54 @@
  */
 int is_prime_number(int n)
 {
+	if (n <= 0)
+		return (0);
+	return (calc(n, 1));
+}
 
+/**
+ * calc - determine if prime
+ * @n: number
+ * @i: divisor
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int calc(int n, int i)
+{
+	if (i > 1 && i < n)
+	{
+		if (n % i == 0)
+			return (0);
+		else if (n % i != 0)
+			return (calc(n, i + 1));
+	}
+	return (1);
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = is_prime_number(1);
+	printf("%d\n", r);
+	r = is_prime_number(1024);
+	printf("%d\n", r);
+	r = is_prime_number(16);
+	printf("%d\n", r);
+	r = is_prime_number(17);
+	printf("%d\n", r);
+	r = is_prime_number(25);
+	printf("%d\n", r);
+	r = is_prime_number(-1);
+	printf("%d\n", r);
+	r = is_prime_number(113);
+	printf("%d\n", r);
+	r = is_prime_number(7919);
+	printf("%d\n", r);
+	return (0);
 }
