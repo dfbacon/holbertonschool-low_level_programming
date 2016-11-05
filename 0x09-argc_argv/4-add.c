5503d3ba7397c2b3353aecdde2df4bfe3ecc89d2
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-05 20:06:15 +0000

    ex4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - add positive numbers
@@ -9,27 +11,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int counter, sum;
+	int i, sum, total;
 
-	if (argc > 0)
-	{
-		if (argv[] > '0' && argv[] < '9')
-		{
-			i = 0;
-			while (i < argc)
-			{
-				sum = argv[i] + argv[i + 1];
-				i++;
-			}
-			return (sum);
-		}
-		else
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-	}
-	else
+	sum = 0;
+	if (argc < 1)
 		printf("%d\n", 0);
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+		total = atoi(argv[argc]);
+		sum += total;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
