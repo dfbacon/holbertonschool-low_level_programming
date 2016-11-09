bf3ccf7833a4741953b8f60ad7e278cd8b55e49c
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-09 19:01:02 +0000

    ex1 check

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strdup - copy a string
@@ -17,6 +18,7 @@ char *_strdup(char *str)
 	i = 0;
 	while (str[i])
 		i++;
+	i++;
 	s = malloc(i * sizeof(char));
 	if (s == NULL)
 		return (NULL);
@@ -27,4 +29,24 @@ char *_strdup(char *str)
 		n++;
 	}
 	return (s);
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s;
+
+	s = _strdup("");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
