ab89e98ed134d4720a93af68da3c99bd91aa33f1
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-09 18:43:00 +0000

    ex1 remove main

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * _strdup - copy a string
@@ -26,24 +25,4 @@ char *_strdup(char *str)
 		n++;
 	}
 	return (s);
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *s;
-
-	s = _strdup("Holberton");
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
 }
