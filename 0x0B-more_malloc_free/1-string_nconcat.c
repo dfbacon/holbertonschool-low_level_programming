149925679ac3599eb219a84566944ee94e3c368c
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-10 21:44:52 +0000

    ex1 change size s1

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -19,29 +19,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-/* iterate to get length of s1, drop the \0 */
+
 	i = 0;
 	while (s1[i])
 		i++;
-/* remove \0 char from s1 */
-	i = i - 1;
-/* allocate memory for size of s1 + n-bytes */
+
 	str = malloc((i + n) * sizeof(*str));
 	if (str == NULL)
 		return (NULL);
-/* as you iterate through s2, add s2[j] to the s1[i] until j == n */
-/* str must contain all of s1 plus n-bytes of s2, don't forget last char must be \0 */
+
 	j = 0;
 	while (s2[j])
 		j++;
-/* set new string (str) to s1 */
+
 	k = 0;
 	while (s1[k])
 	{
 		str[k] = s1[k];
 		k++;
 	}
-/* concatenate all of s2 to s1  */
+
 	if (j <= n)
 	{
 		l = 0;
@@ -53,7 +50,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		str[k] = '\0';
 	}
-/* concatenate n-bytes of s2 to s1 */
+
 	else if (j > n)
 	{
 		l = 0;
