cd3e990d18269c06efd132caa7f51b8042170312
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-10 02:06:10 +0000

    ex5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -12,11 +12,60 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
+	int i, n, t, l;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	/* maybe sizeof(void *) */
-	str = malloc(ac * sizeof(char*));
-	/* more code here */
+	i = 0;
+	l = 0;
+	while (i < ac)
+	{
+		n = 0;
+		while (av[i][n])
+		{
+			l++;
+			n++;
+		}
+		l++;
+		i++;
+	}
+	l++;
+	str = malloc(ac * sizeof(*str));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	t = 0;
+	while (i < ac)
+	{
+		n = 0;
+		while (av[i][n])
+		{
+			str[t] = av[i][n];
+			t++;
+			n++;
+		}
+		str[t] = '\n';
+		t++;
+		i++;
+	}
 	return (str);
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
+{
+	char *s;
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
 }
