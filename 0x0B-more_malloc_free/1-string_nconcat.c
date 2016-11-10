2792f46f8593a84de21fbc6d9aa49ec1f1da1ac6
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-10 21:54:45 +0000

    ex1 code check

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
 
@@ -19,26 +18,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	i = 0;
 	while (s1[i])
 		i++;
-
-	str = malloc((i + n) * sizeof(*str));
+	str = malloc((i + n) * sizeof(*str) + 1);
 	if (str == NULL)
 		return (NULL);
-
 	j = 0;
 	while (s2[j])
 		j++;
-
 	k = 0;
 	while (s1[k])
 	{
 		str[k] = s1[k];
 		k++;
 	}
-
 	if (j <= n)
 	{
 		l = 0;
@@ -50,7 +44,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		str[k] = '\0';
 	}
-
 	else if (j > n)
 	{
 		l = 0;
