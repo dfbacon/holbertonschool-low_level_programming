406507d3a300a9d9f106e0b0ce7ea3375bcfc275
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-10 22:08:15 +0000

    ex3 clean up, first try with comments

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include "holberton.h"
 
@@ -41,47 +39,4 @@ int *array_range(int min, int max)
 /* add \0 to the end?? */
 	array[counter] = '\0';
 	return (array);
-}
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int *a;
-
-	a = array_range(0, 10);
-	simple_print_buffer(a, 11);
-	free(a);
-	return (0);
 }
