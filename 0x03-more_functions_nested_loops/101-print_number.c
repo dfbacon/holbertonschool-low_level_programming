41233e71c26fff781ee858b317e00653fd727191
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-11 21:32:51 +0000

    ex101

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_number - ptint an integer
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i;
+
+	i = 1000000000;
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	else if (n == 0)
+		_putchar('0');
+	while (i > 0)
+	{
+		if (n >= i)
+		{
+			if (i >= 10)
+				_putchar((n / i) % 10 + '0');
+			else
+			{
+				_putchar(n %10 + '0');
+				n = 0;
+			}
+		}
+		if (i > 1)
+			i = i /10;
+		else
+			i = 0;
+	}
+}
