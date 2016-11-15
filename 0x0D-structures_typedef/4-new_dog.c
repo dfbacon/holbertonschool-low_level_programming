69ad9446f3f0068d03d0f3bdc4a5b02aae87c699
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-15 23:37:06 +0000

    ex4 fix

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,27 +11,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
-	int len_1, len_2, i;
+	dog_t *d;
+	int len_name, len_owner, i;
 
-	len_1 = len_2 = i = 0;
-	while (name[len_1])
-		len_1++;
-	while (owner[len_2])
-		len_2++;
+	len_name = len_owner = i = 0;
+	while (name[len_name++] != '\0')
+		;
+	while (owner[len_owner++] != '\0')
+		;
 
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
-	ptr->name = malloc(len_1);
-	ptr->owner = malloc(len_2);
+
+	d->name = malloc(len_name);
+	d->owner = malloc(len_owner);
+
 	do {
-		ptr->name[i] = name[i];
-	} while (name[i++]);
+		d->name[i] = name[i];
+	} while (name[i++] != '\0');
 	i = 0;
 	do {
-		ptr->owner[i] = owner[i];
-	} while (owner[i++]);
-	ptr->age = age;
-	return (ptr);
+		d->owner[i] = owner[i];
+	} while (owner[i++] != '\0');
+	d->age = age;
+	return (d);
 }
