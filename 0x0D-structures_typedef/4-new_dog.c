a19b8902e00d46c4d0ed7e899d53cbafa2ec6a20
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-15 23:40:45 +0000

    ex4 trying something

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,29 +11,13 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	int len_name, len_owner, i;
+	struct dog *new;
 
-	len_name = len_owner = i = 0;
-	while (name[len_name++] != '\0')
-		;
-	while (owner[len_owner++] != '\0')
-		;
-
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
+	new = malloc(sizeof(struct dog));
+	if (new == NULL)
 		return (NULL);
-
-	d->name = malloc(len_name);
-	d->owner = malloc(len_owner);
-
-	do {
-		d->name[i] = name[i];
-	} while (name[i++] != '\0');
-	i = 0;
-	do {
-		d->owner[i] = owner[i];
-	} while (owner[i++] != '\0');
-	d->age = age;
-	return (d);
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+	return (new);
 }
