e158c79222a26e50ae73363b4a934a970e8f9d99
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-15 23:23:33 +0000

    ex4 _strdup add

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,5 +1,35 @@
 #include "dog.h"
+#include "holberton.h"
 #include <stdlib.h>
+
+/**
+ * _strdup - copy a string
+ * @str: string
+ *
+ * Return: pointer to copy
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i, n;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+		i++;
+	s = malloc((i * sizeof(char)) + 1);
+	if (s == NULL)
+		return (NULL);
+	n = 0;
+	while (n < i)
+	{
+		s[n] = str[n];
+		n++;
+	}
+	s[n] = '\0';
+	return (s);
+}
 
 /**
  * new_dog - create a new dog structure
@@ -13,11 +43,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 
-	ptr = malloc(sizeof(dog_t));
+	ptr = malloc(sizeof(struct dog_t));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = name;
+	ptr->name = _strdup(name);
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	ptr->age = age;
-	ptr->owner = owner;
+	ptr->owner = _strdup(owner);
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
 	return (ptr);
 }
