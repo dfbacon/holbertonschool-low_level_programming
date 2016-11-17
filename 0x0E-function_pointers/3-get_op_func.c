c147becfb81eaaac3d86534759c51acbba432558
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-17 23:17:46 +0000

    ex3 op functions

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -10,6 +10,7 @@
 
 int (*get_op_func(char *s))(int a, int b)
 {
+	int i;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -18,7 +19,6 @@ int (*get_op_func(char *s))(int a, int b)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
 
 	i = 0;
 	while (i < 5)
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+ *op_add - returns the sum of a and b
+ *@a: num1
+ *@b: num2
+ *
+ *Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - returns the difference of a and b
+ *@a: num1
+ *@b: num2
+ *
+ *Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - returns the product of a and b
+ *@a: num1
+ *@b: num2
+ *
+ *Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - returns the result of the division of a by b
+ *@a: num1
+ *@b: num2
+ *
+ *Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - returns the remainder of the division of a by b
+ *@a: num1
+ *@b: num2
+ *
+ *Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
