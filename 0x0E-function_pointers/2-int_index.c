c20d4347f4b6d4bb7707753eeec2f15237ef777f
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-17 19:39:23 +0000

    ex2 better NULL case

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -13,14 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	i = 0;
 	while (i < size)
 	{
 		cmp(array[i]);
-		if (cmp(array[i]) == NULL)
-			return (-1);
 		if (cmp(array[i]) != 0)
 			return (i);
 		i++;
