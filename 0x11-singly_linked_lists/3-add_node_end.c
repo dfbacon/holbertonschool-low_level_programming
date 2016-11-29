06bba1c948d74de8b5d958b6e37c2a207590511b
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-29 00:11:30 +0000

    ex3 betty fixes

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -27,6 +27,7 @@ int _strlen(const char *s)
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *temp;
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
@@ -42,5 +43,5 @@ list_t *add_node_end(list_t **head, const char *str)
 			temp = temp->next;
 		temp->next = new;
 	}
-	return(new);
+	return (new);
 }
