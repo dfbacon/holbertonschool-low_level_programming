c2ec88ee2f20de0fae273c8c7ba753355d3c8f93
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-29 20:39:22 +0000

    ex101 add call to printf

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,12 @@
-section .data	; Data section, initialized variables
-
-	msg db "Hello Holberton", 0 ; C string needs 0
+	section .data	; Data section, initialized variables
 	fmt db "%s", 10, 0      ; The printf format, "\n",'0'
-	section .text	; Code section.
-	global main	; the standard gcc entry point
-main				; the program label for the entry point
-	push    rbp	; set up stack frame, must be alligned
+	msg db "Hello Holberton", 0 ; C string needs 0
 
+	section .text	; Code section.
+	extern printf	; Call to printf
+	global main	; the standard gcc entry point
+main:			; the program label for the entry point
+	push    rbp	; set up stack frame, must be alligned
 	mov	rdi,fmt
 	mov	rsi,msg
 	mov	rax,0	; or can be  xor  rax,rax
