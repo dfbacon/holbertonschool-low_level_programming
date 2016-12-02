613032e8d130232adb3a7c76f7d581ffe75bf5ba
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-02 21:12:53 +0000

    header fix

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -37,8 +37,8 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-size_t check_node(const listptr_t **head, const listptr_t *n);
-size_t free_node(const listptr_t *head);
+size_t check_node(listptr_t **head, const listptr_t *n);
+void free_node(listptr_t *head);
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
