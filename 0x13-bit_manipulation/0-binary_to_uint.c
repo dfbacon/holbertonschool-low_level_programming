7ed743b47e62caef9c1b64b6ec36e18ab9be77ad
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-05 19:51:37 +0000

    ex1

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to string containing 0s and 1s
+ *
+ * Return: converted unsigned int, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int u, bit_value;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		/* looking at the ASCII values of 0 and 1 */
+		if (!(*b == 48 || *b == 49))
+			return (0);
+		b++;
+	}
+	b--;
+	u = 0;
+	bit_value = 1;
+	while(*b)
+	{
+		if (*b == 48) /* if *b == 0 */
+			u += 0;
+		else if (*b == 49) /* if *b == 1 */
+			u += (1 * bit_value);
+		bit_value *= 2;
+		b--;
+	}
+	return (u);
+}
