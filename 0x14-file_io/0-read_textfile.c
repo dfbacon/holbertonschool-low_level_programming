81629f87cbeed79fa0d814421cf63f5db966cbbf
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 02:32:52 +0000

    ex0 read and write

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -14,14 +14,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	if (letters == 0)
-		return (0);
 
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
 
