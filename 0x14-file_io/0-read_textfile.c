0729e88eec50a6868606194e5b23e97e569775e7
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 02:17:08 +0000

    ex0 cleaned up

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h> /* remove me */
 
 /**
  * read_textfile - reads a text file and print it to the POSIX standard output
@@ -32,25 +31,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	free(buf);
 	return (rfd);
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(int ac, char **av)
-{
-	ssize_t n;
-
-	if (ac != 2)
-	{
-		dprintf(2, "Usage: %s filename\n", av[0]);
-		exit(1);
-	}
-	n = read_textfile(av[1], 114);
-	printf("\n(printed chars: %li)\n", n);
-	n = read_textfile(av[1], 1024);
-	printf("\n(printed chars: %li)\n", n);
-	return (0);
 }
