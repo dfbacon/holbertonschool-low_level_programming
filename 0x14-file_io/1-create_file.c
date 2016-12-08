dabb7c26fb82d45b3713f46d010c92b3d60ab12b
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 06:42:12 +0000

    ex1 change permissions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	if (buf == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		free(buf);
