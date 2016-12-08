65cdef189510ba7a049730454b5fecee1134a0b0
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 20:47:05 +0000

    betty fixes

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -17,12 +17,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	if (argv[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]), exit(98);
 	if (argv[2] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	rfd = open(argv[1], O_RDONLY);
 	if (rfd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]), exit(98);
 	wfd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (wfd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -31,7 +33,8 @@ int main(int argc, char *argv[])
 	{
 		i = read(rfd, buf, BUFFERSIZE);
 		if (i == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
 		if (i > 0)
 		{
 			cfd = write(wfd, buf, i);
