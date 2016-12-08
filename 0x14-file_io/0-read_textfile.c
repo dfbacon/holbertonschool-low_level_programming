8a9e61fa55c429b7d5112f6e1991e032dbd6a36c
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 02:20:52 +0000

    ex0 no printf

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (rfd == -1)
 		return (0);
 	else
-		printf("%s", buf);
+		write(1, buf, letters);
 	close(fd);
 	free(buf);
 	return (rfd);
