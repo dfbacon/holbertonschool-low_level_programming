c8464ada967fb4286fbaad1e890d1568f1d71561
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 03:58:32 +0000

    ex1 and ex2 shooting in the dark

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -28,9 +28,9 @@ int create_file(const char *filename, char *text_content)
 	int fd, len;
 	char *buf;
 
+	if (filename == NULL)
+		return (-1);
 	len = _strlen(text_content);
-	if (text_content == NULL)
-		text_content = "";
 	buf = malloc(sizeof(char) * len);
 	if (buf == NULL)
 		return (-1);
@@ -41,6 +41,8 @@ int create_file(const char *filename, char *text_content)
 		free(buf);
 		return (-1);
 	}
+	else if (text_content == NULL)
+		write(fd, "", len);
 	else
 		write(fd, text_content, len);
 	close(fd);
