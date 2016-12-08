4336c8bd549ed45d77dec369f9c6ba8f1c869619
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 02:54:28 +0000

    ex3 framework

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * main - copy contents of a file to another file
+ * @argc: agrument counter
+ * @argv: pointer to array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+}
