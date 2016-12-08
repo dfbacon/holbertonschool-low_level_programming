4cf4d7b26128cc1a0547cd9fc18a2cdb01b5fd89
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 22:40:02 +0000

    close and free

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -21,16 +21,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	rfd = read(fd, buf, letters);
 	if (rfd == -1)
+	{
+		close(fd);
+		free(buf);
 		return (0);
+	}
 	wfd = write(STDOUT_FILENO, buf, rfd);
 	if (wfd == -1)
+	{
+		close(fd);
+		free(buf);
 		return (0);
+	}
 	cfd = close(fd);
 	if (cfd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	free(buf);
 	return (rfd);
 }
