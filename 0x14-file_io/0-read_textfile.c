474d3498f8c9d8d2d77f1f472a89cf792b882660
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 02:57:53 +0000

    ex0 rfd = 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	rfd = read(fd, buf, letters);
-	if (rfd == -1)
+	if (rfd == -1 || rfd == 0)
 		return (0);
 	write(1, buf, rfd);
 	close(fd);
