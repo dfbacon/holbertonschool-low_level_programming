c15827227d3d9eb370bef001c43f24c988d2e043
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-08 03:53:10 +0000

    ex2 RDWR

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -35,7 +35,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (buf == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_APPEND | O_RDWR);
 	if (fd == -1)
 	{
 		free(buf);
