6dfb3bc93396f5d0004facb6ad1b6d267f07b4f5
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-09 23:14:55 +0000

    ex7 back to square one

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,35 +14,41 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *target;
 	unsigned int i;
 
-	if (h == NULL)
+	if (*h == NULL)
 		return (NULL);
+	target = *h; i = 0;
+	while (target->next)
+		target = target->next; i++;
+	if (idx > i)
+		return (NULL);
+	target = *h; i = 0;
+	if (idx > 0)
+	{
+		while (i < idx - 1)
+		{
+			if (*h == NULL)
+				return (NULL);
+			target = target->next; i++;
+		}
+	}
 	new = malloc(sizeof(*new));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	i = 0;
-	target = *head;
-	while (i < index - 1)
+	if (idx == 0)
 	{
-		target = target->next;
-		if (target == NULL)
-			return (NULL);
-		i++;
-	}
-	if (index == 0)
-	{
-		*head = new;
 		new->next = target;
+		if (*h == NULL)
+			new->next = NULL;
+		new->prev = NULL;
+		if (*h)
+			target->prev = new;
+		*h = new;
+		return (new);
 	}
-	else if (target->next)
-	{
-		new->next = target->next;
-		target->next = new;
-	}
-	else
-	{
-		new->next = NULL;
-		target->next = new;
-	}
+	new->next = target->next;
+	(target->next)->prev = new;
+	target->next = new;
+	new->prev = target;
 	return (new);
 }
