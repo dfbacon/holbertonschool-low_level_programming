f3b6c7b93e0dcb1012b945cb479f1a9444a2ceb9
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-09 22:55:35 +0000

    ex7 empty list check edit

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *target;
 	unsigned int i;
 
-	if (*h == NULL && idx != 0)
+	if (*h == NULL)
 		return (NULL);
 	target = *h; i = 0;
 	while (target->next)
