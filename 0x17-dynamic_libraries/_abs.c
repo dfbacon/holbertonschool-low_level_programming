80520f97e1fabccf5f901f6c993d05b9bd662e44
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-01-14 20:47:31 +0000

    pushing c files for ex1

---
+++ 0x17-dynamic_libraries/_abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @i: interger to computer
+ *
+ * Return: absolute value;
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+		return (i);
+	}
+	else
+		return (i);
+}
---
+++ 0x17-dynamic_libraries/_atoi.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert string to integer
+ * @s: string to convert
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int number, i, neg;
+
+	neg = 1;
+	i = number = 0;
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			neg = neg * -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (number >= 0)
+		{
+			number = number * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			number = number * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	neg = neg * -1;
+	return (number * neg);
+}
