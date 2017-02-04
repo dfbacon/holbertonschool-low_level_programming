0ba77746ff53cc7d892b1958d17c361827b8e0e3
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-04 22:22:30 +0000

    ex5

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,6 +1,29 @@
 #include "hash_tables.h"
 
 /**
+ * ht_print_helper - helper function to print key/value pairs in hash table
+ *
+ * @node: node containing key/value pair to print
+ * @i: node counter
+ * Return: always @i
+ */
+int ht_print_helper(hash_node_t *node, int i)
+{
+	if (node == NULL)
+		return (i);
+
+	while (node)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("'%s': '%s'", node->key, node->value);
+		i++;
+		node = node->next;
+	}
+	return (i);
+}
+
+/**
  * hash_table_print - print a hash table
  *
  * @ht: hash table to print
@@ -11,4 +34,17 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *head;
 	unsigned long int index;
 	int i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	index = 0;
+	i = 0;
+	while (index < ht->size)
+	{
+		head = ht->array[index];
+		i = ht_print_helper(head, i);
+	}
+	printf("}\n");
 }
