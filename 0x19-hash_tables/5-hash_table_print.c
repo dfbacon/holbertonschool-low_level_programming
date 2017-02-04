56c1c026dd9f62a5a2c0b4d8d9bc2476d3fcd719
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-04 21:52:17 +0000

    ex5

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ *
+ * @ht: hash table to print
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *head;
+	unsigned long int index;
+	int i;
+}
