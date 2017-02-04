a7609d0c72f487f3052275e70255c13ae20a176f
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-04 23:19:48 +0000

    ex6

--- 0x19-hash_tables/6-hash_table_print.c
+++ 0x19-hash_tables/6-hash_table_print.c
@@ -8,4 +8,17 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_table_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	temp = ht;
+	while (temp)
+	{
+		temp = ht;
+		ht = ht->next;
+		free(temp->array);
+		free(temp);
+	}
 }
