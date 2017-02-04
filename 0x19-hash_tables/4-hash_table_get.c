3fb902a267c60046d557adb59d87e4c55d60ce30
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-04 21:40:35 +0000

    ex4 framework

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve a value associated with a key
+ *
+ * @ht: hash table containing @key
+ * @key: key to look for to find associated value
+ * Return: the value associated with @key, or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+}
