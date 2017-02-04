40f19622d57c6c62a4faf5d538d61061fa5a5c6d
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-04 20:49:28 +0000

    ex3

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element to the hash table
+ * @ht: hash table you want to add or update the key/value to
+ * @key: the key
+ * @value: value paired with @key
+ *
+ * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
