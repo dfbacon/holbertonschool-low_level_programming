2a2ca75611a1717bdf3b3a75fc073e44f0e90c89
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-04 23:13:58 +0000

    ex6 framework

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -39,6 +39,8 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *hash_table_helper(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+int ht_print_helper(hash_node_t *node, int i);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_H */
