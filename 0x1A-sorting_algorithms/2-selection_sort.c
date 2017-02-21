d6a52fbbf11d45eb41b808479c3be531884434e1
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-21 21:50:32 +0000

    variable swap for consistency

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -16,18 +16,18 @@ void selection_sort(int *array, size_t size)
 	if (size < 1)
 		return;
 
-	for (j = 0; j < size - 1; j++)
+	for (i = 0; i < size - 1; i++)
 	{
-		min = j;
-		for (i = j + 1; i < size; i++)
+		min = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] < array[min])
-				min = i;
+			if (array[j] < array[min])
+				min = j;
 		}
-		if (min != j)
+		if (i != min)
 		{
-			temp = array[j];
-			array[j] = array[min];
+			temp = array[i];
+			array[i] = array[min];
 			array[min] = temp;
 			print_array(array, size);
 		}
