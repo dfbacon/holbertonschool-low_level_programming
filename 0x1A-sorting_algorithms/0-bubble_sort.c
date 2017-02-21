6a888342e38d8bf16e572585a4444dfcc1062a47
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-21 21:45:11 +0000

    move flag initialization

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -13,20 +13,19 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, flag;
 	int temp;
 
+	flag = 0;
 	if (size <= 1)
 		return;
-
-	flag = 0;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				flag = 1;
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
