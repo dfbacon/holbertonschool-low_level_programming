1adfe8c390bc968a6814ef75fb8a131f998c2225
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-21 21:54:13 +0000

    remove null condition

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -1,22 +1,17 @@
 #include "sort.h"
 
 /**
- * selection_sort - sort an arraw of integers in ascending order
- * @array: array of integers to sort
- * @size: size of the array
+ * selection_sort - sort an array of integers using selection sort algorithm
+ * @array: array of integers
+ * @size: size of @array
  *
- * Return: void
+ * Return: voidy
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min, temp;
 
-	if (array == NULL)
-		return;
-	if (size <= 1)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1 ; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
