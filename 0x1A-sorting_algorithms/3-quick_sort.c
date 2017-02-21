15f68598b77d007393046fa22de94c229b489dd0
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-21 04:54:07 +0000

    re-initalize sort helper function

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -7,13 +7,14 @@
  *
  * Return: void
  */
-void quick_swap(int *array, int first, int second)
+void quick_swap(int *array, int first, int second, size_t size)
 {
-	size_t temp;
+	int temp;
 
 	temp = array[first];
 	array[first] = array[second];
 	array[second] = temp;
+	print_array(array, size);
 }
 
 /**
@@ -23,7 +24,7 @@ void quick_swap(int *array, int first, int second)
  *
  * Return: pivot in center position
  */
-int set_pivot(int *array, size_t size)
+int set_pivot(int *array, int start, int stop, size_t size)
 {
 	size_t up, down;
 	int partition;
@@ -41,12 +42,32 @@ int set_pivot(int *array, size_t size)
 			down--;
 		if (up >= down)
 			break;
-		quick_swap(array, up, down);
+		quick_swap(array, up, down, size);
 		up++;
 		down--;
 	}
-	quick_swap(array, up, down);
+	quick_swap(array, up, down, size);
 	return (up);
+}
+
+/**
+ * sort - helper function for quick_sort
+ * @array: array of integers to sort
+ * @start: left-most element of @array
+ * @stop: right-most element of @array
+ * @size: size of @array
+ *
+ * Return: void
+ */
+void sort(int *array, int start, int stop, size_t size)
+{
+	int partition;
+
+	if(end - start < 2)
+		return;
+	partition = set_pivot(array, start, stop, size);
+	sort(array, start, partition, size);
+	sort(array, partition, stop, size);
 }
 
 /**
@@ -60,10 +81,5 @@ void quick_sort(int *array, size_t size)
 {
 	int pivot;
 
-	printf("entering sort\n");
-	if (array[0] <= array[size - 1])
-		return;
-	pivot = set_pivot(array, size);
-	set_pivot(array, size - 1);
-	(void) pivot;
+	sort(array, 0, size - 1, size);
 }
