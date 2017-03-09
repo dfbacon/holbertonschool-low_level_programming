4a7ae8452171ea4fa47f19bd1fb13782c9eccdd7
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-09 22:41:44 +0000

    create binary tree node

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -8,4 +8,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new_node);
 }
