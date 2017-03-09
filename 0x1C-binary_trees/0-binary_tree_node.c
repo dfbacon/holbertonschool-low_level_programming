efca6d79167c95113c6039b34cb67d00d750f614
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-09 22:28:34 +0000

    add comments

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a binary_tree_t node with no children
+ * @parent: pointer to parent node of node to create
+ * @value: value of new node
+ *
+ * Return: pointer to new node, or NULL if fail
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
