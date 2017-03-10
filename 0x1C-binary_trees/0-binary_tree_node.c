277f3cd438d437d450d04b17b357d078059536ac
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-10 06:52:17 +0000

    change new to new_node

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -14,9 +14,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	return (new_node);
 }
