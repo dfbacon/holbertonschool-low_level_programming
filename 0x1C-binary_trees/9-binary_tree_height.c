896788647ab37a9a960e6530b45d089cbae0b1ed
Daniel Bacon <dbacon338@gmail.com>
2017-03-20 20:58:25 +0000

    change tree is null check

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -10,18 +10,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int height_right, height_left;
 
+	if (tree == NULL)
+		return (0);
 	if (tree && (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	if (tree)
-	{
-		height_left = binary_tree_height(tree->left);
-		height_right = binary_tree_height(tree->right);
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
 
-		if (height_left > height_right)
-			return (height_left + 1);
-		else
-			return (height_right + 1);
-	}
-	return (-1);
+	if (height_left > height_right)
+		return (height_left + 1);
+	else
+		return (height_right + 1);
 }
