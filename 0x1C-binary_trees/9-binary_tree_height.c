571ea722027e7d3cb709f82affc8d3059a666047
Daniel Bacon <dbacon338@gmail.com>
2017-03-20 18:30:22 +0000

    empty tree edge case

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -10,6 +10,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int height_right, height_left;
 
+	if (tree && (tree->left == NULL && tree->right == NULL))
+		return (0);
+
 	if (tree)
 	{
 		height_left = binary_tree_height(tree->left);
